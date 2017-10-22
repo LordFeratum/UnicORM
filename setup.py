e95dc165680ed70ad2166c5d1bcc2d623e2efe0a
Quique Piña <lord.feratum@gmail.com>
2017-10-22 15:26:54 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='sqlchemistry',
+    packages=['sqlchemistry'],
+    version='0.0.1',
+    description='A simple asynchronous ORM',
+    author='Enrique Piña Monserrat',
+    author_email='lord.feratum@gmail.com',
+    url='https://github.com/LordFeratum/SQLChemistry',
+    download_url='https://github.com/LordFeratum/SQLChemistry/archive/0.0.1.tar.gz', # noqa
+    keywords=['async', 'orm', 'mysql'],
+    classifiers=[],
+)
